17270b15b4538a32424b12e3a0a76ec49010b557
kempsh661 <kempsh661@gmail.com>
2025-10-07 11:30:29 -0500

    Fix: Agregar import de DB facade en CreditController para solucionar error en módulo de créditos

--- app/Http/Controllers/CreditController.php
+++ app/Http/Controllers/CreditController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Sale;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class CreditController extends Controller
 {
